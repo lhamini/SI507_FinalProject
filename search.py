af73309a9fd6b84e9fc1fefebaf1b3c93500e7e4
eamini <eamini@umich.edu>
2020-12-10 20:04:32 -0500

    UPD: lametize

--- search.py
+++ search.py
@@ -1,9 +1,17 @@
 import json
-import pandas as pd
+import nltk
 import query
+import pandas as pd
 from flask import request
+from nltk.corpus import stopwords
+from nltk.stem import 	WordNetLemmatizer
+from nltk.stem import PorterStemmer
 from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 CACHE_FILENAME = "doc_cache.json"
+S = set(stopwords.words('english'))
+
+wordnet_lemmatizer = WordNetLemmatizer()
+ps =PorterStemmer()
 
 def save_cache(cache_dict):
     dumped_json_cache = json.dumps(cache_dict)
@@ -23,9 +31,18 @@ def open_cache():
     return cache_dict
 
 
+def remove_stopwords(tokens):
+    tokens_stop_removed = []
+    for token in tokens:
+        #token = wordnet_lemmatizer.lemmatize(token)
+        token = ps.stem(token)
+        if not token.lower() in S:
+            tokens_stop_removed.append(token)
+    return tokens_stop_removed
+
 def corpus_index():
     cache_dict = open_cache()
     corpus = list(cache_dict.values())
-    tokenized_corpus = [str(doc).split(" ") for doc in corpus]
+    tokenized_corpus = [remove_stopwords(str(doc).split(" ")) for doc in corpus]
     bm25plus = BM25Okapi(tokenized_corpus)
     return corpus, bm25plus, cache_dict
